ca142920fdfcbf7a3145c63cebe3ac851676769a
Алиев Айдемир Камильевич <aydik@MacBook-Air-Aliev.local>
2023-11-07 08:31:21 +0300

    s

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
         <div className={'header'}>
             <Button onClick={onClose}>Close</Button>
             <span className={'username'}>
-                {user?.username}
+                {/* {user?.username} */}
             </span>
         </div>
     )
