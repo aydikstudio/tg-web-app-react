ab60727715bf64d2626d1079457a2f0fc17a07c5
Алиев Айдемир Камильевич <aydik@MacBook-Air-Aliev.local>
2023-11-07 08:30:11 +0300

    s

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
         <div className={'header'}>
             <Button onClick={onClose}>Close</Button>
             <span className={'username'}>
-                {tg.initDataUnSafe?.user?.username}
+                {user?.username}
             </span>
         </div>
     )
