8979f3c9a87a27c2394c82da580525d700e30c76
Алиев Айдемир Камильевич <aydik@MacBook-Air-Aliev.local>
2023-11-12 11:47:34 +0300

    s

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -8,6 +8,23 @@ const Form = () => {
     const [subject, setSubject] = useState('physical');
     const {tg} = useTelegram();
 
+    const onSendData = useCallback(() => {
+        const data = {
+            country,
+            street,
+            subject
+        }
+
+        tg.sendData(JSON.stringify(data))
+    }, [country, street, subject])
+
+    useEffect(() => {
+        tg.onEvent('mainButtonClicked', onSendData)
+        return () => {
+            tg.offEvent('mainButtonClicked', onSendData)
+        }
+    }, [])
+
     useEffect(() => {
         tg.MainButton.setParams({
             text: 'Send data'
@@ -44,13 +61,7 @@ const Form = () => {
         })
     }, [])
 
-    useEffect(() => {
-        if(!street || !country) {
-            tg.MainButton.hide();
-        } else {
-            tg.MainButton.show();
-        }
-    }, [country, street])
+ 
 
     const onChangeCountry = (e) => {
         setCountry(e.target.value)
