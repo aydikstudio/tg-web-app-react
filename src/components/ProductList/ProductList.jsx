7e25acabe7eadf0d15faae3911ffebac92c2c64c
Алиев Айдемир Камильевич <aydik@MacBook-Air-Aliev.local>
2023-11-12 22:30:38 +0300

    s

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -33,7 +33,7 @@ const ProductList = () => {
             queryId
         }
        
-        fetch('https://5.35.13.13:8000/web-data', {
+        fetch('https://aydikstudio.ru/project/bot/tg-web:8000/web-data', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
