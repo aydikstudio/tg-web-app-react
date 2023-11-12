514191a44db6bbfde5502f0598ac5b72ed4e4304
Алиев Айдемир Камильевич <aydik@MacBook-Air-Aliev.local>
2023-11-12 14:12:09 +0300

    s

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -31,7 +31,7 @@ const ProductList = () => {
             totalPrice: getTotalPrice(addedItems)
         }
 
-        fetch('http://localhost:8000', {
+        fetch('http://192.168.0.114:8000/web-data', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
